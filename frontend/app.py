04d4f2bd6c27a5f3b33a7d5ef17b996205b806b6
welltone <78064037+welltone@users.noreply.github.com>
2026-07-26 03:04:37 +0900

    Add frontend/app.py

---
+++ frontend/app.py
@@ -0,0 +1,81 @@
+
+import streamlit as st
+import requests
+
+st.title("Superkart Sales Prediction")
+
+# Input fields for product and store data
+Product_Weight = st.number_input("Product Weight", min_value=4.0, max_value=22.0, value=12.66)
+Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
+Product_Allocated_Area = st.number_input("Product Allocated Area", min_value=0.004, max_value=0.298, value=0.056, format="%.3f")
+Product_MRP = st.number_input("Product MRP", min_value=31.0, max_value=266.0, value=146.74)
+Store_Size = st.selectbox("Store Size", ["Medium", "High", "Small"])
+Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 2", "Tier 1", "Tier 3"])
+Store_Type = st.selectbox("Store Type", ["Supermarket Type2", "Departmental Store", "Supermarket Type1", "Food Mart"])
+Product_Id_char = st.selectbox("Product ID Code", ["FD", "NC", "DR"])
+Store_Age_Years = st.number_input("Store Age Years", min_value=16, max_value=38, value=16)
+Product_Type_Category = st.selectbox("Product Type Category", ["Non Perishables", "Perishables"])
+
+product_data = {
+    "Product_Weight": Product_Weight,
+    "Product_Sugar_Content": Product_Sugar_Content,
+    "Product_Allocated_Area": Product_Allocated_Area,
+    "Product_MRP": Product_MRP,
+    "Store_Size": Store_Size,
+    "Store_Location_City_Type": Store_Location_City_Type,
+    "Store_Type": Store_Type,
+    "Product_Id_char": Product_Id_char,
+    "Store_Age_Years": Store_Age_Years,
+    "Product_Type_Category": Product_Type_Category
+}
+
+if st.button("Predict", type='primary'):
+    response = requests.post("https://<user_name>-<space_name>.hf.space/v1/predict", json=product_data)
+    if response.status_code == 200:
+        result = response.json()
+        predicted_sales = result["Sales"]
+        st.write(f"Predicted Product Store Sales Total: ₹{predicted_sales:.2f}")
+    else:
+        st.error("Error in API request")
+"""
+%%writefile frontend_files/app.py
+
+import streamlit as st
+import requests
+
+st.title("_____") #Complete the code to define the title of the app.
+
+# Input fields for product and store data
+Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
+Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
+Product_Allocated_Area = ______ #Complete the code to define the UI element for Product_Allocated_Area
+Product_MRP = ______ #Complete the code to define the UI element for Product_MRP
+Store_Size = _____ ______ #Complete the code to define the UI element for Store_Size
+Store_Location_City_Type = _____ ______ #Complete the code to define the UI element for Store_Location_City_Type
+Store_Type = _____ ______ #Complete the code to define the UI element for Store_Type
+Product_Id_char = _____ ______ #Complete the code to define the UI element for Product_Id_char
+Store_Age_Years = _____ ______ #Complete the code to define the UI element for Store_Age_Years
+Product_Type_Category = _____ ______ #Complete the code to define the UI element for Product_Type_Category
+
+product_data = {
+    "Product_Weight": Product_Weight,
+    "Product_Sugar_Content": Product_Sugar_Content,
+    "Product_Allocated_Area": Product_Allocated_Area,
+    "Product_MRP": Product_MRP,
+    "Store_Size": Store_Size,
+    "Store_Location_City_Type": Store_Location_City_Type,
+    "Store_Type": Store_Type,
+    "Product_Id_char": Product_Id_char,
+    "Store_Age_Years": Store_Age_Years,
+    "Product_Type_Category": Product_Type_Category
+}
+
+if st.button("Predict", type='primary'):
+    response = requests.post("https://<user_name>-<space_name>.hf.space/v1/predict", json=product_data)    # Complete the code to enter user name and space name to correctly define the endpoint
+    if response.status_code == 200:
+        result = response.json()
+        predicted_sales = result["Sales"]
+        st.write(f"Predicted Product Store Sales Total: ₹{predicted_sales:.2f}")
+    else:
+        st.error("Error in API request")
+        """
